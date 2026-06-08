5c4f31b0352b88a2181d79f790460abebaf1d7c1
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-06-08 12:56:18 +0400

    i18 editing

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
-""" Route module for the API - Get locale from request"""
-
+"""Route module for API - Get locale from request"""
 
 from flask import Flask, request, render_template
 from flask_babel import Babel
 from os import getenv
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
-    """ Setup - Babel configuration """
     LANGUAGES = ['en', 'fr']
-    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# set the above class object as the configuration for the app
-app.config.from_object('2-app.Config')
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+# ✅ NEW WAY (NO DECORATOR)
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# Flask-Babel 3+ way:
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """ GET /
-    Return: 2-index.html
-    """
     return render_template('2-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """ Determines best match for supported languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
