b68d4136e17ab94e24e0d3ec428ced32086922ac
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-06-08 12:59:09 +0400

    i18 editing

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 
 class Config(object):
     """Setup - Babel configuration"""
-
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
