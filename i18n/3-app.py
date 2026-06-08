3f17c8a260324dee95f75f721518df74aaa95fed
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-06-08 15:23:39 +0400

    i18 editing

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python3
-"""Route module for the API"""
-
 from flask import Flask, request, render_template
 from flask_babel import Babel
 from os import getenv
 
 app = Flask(__name__)
 
-
-class Config(object):
-    """Babel configuration"""
+class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
 app.config.from_object(Config)
 
-
-# Babel setup
 babel = Babel()
 
-
 def get_locale():
-    """Select best language from request headers"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 
 babel.init_app(app, locale_selector=get_locale)
 
+# 🔥 FIX
+app.jinja_env.globals['_'] = babel.gettext
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """Render home page"""
+def index():
     return render_template('3-index.html')
 
 
