bcb4e2f2b319d9940c723d31634564566e780ad0
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-06-08 15:38:54 +0400

    i18 editing

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -15,7 +15,7 @@ users = {
 }
 
 app = Flask(__name__)
-babel = Babel(app)
+babel = Babel()
 
 
 class Config(object):
@@ -38,21 +38,21 @@ def index() -> str:
     return render_template('6-index.html')
 
 
-@babel.localeselector
 def get_locale() -> str:
     """ Determines best match for supported languages """
     # check if there is a locale parameter/query string
-    if request.args.get('locale'):
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     # check if there is a locale in an existing user's profile
-    elif g.user and g.user.get('locale')\
+    if g.user and g.user.get('locale')\
             and g.user.get('locale') in app.config['LANGUAGES']:
         return g.user.get('locale')
     # default to return as a failsafe
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 def get_user() -> Union[dict, None]:
