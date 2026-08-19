9932816cdaa7a0a83f8783351538d67146d42f74
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-08-19 19:53:19 +0400

    user_authentication_service tasks update

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-''' Module data base '''
+"""Module data base."""
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from user import Base, User
-from typing import TypeVar
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
 
 
 class DB:
+    """DB class for database operations."""
 
     def __init__(self):
-        ''' def init '''
+        """Initialize the database."""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -20,23 +20,27 @@ class DB:
 
     @property
     def _session(self):
-        ''' def session '''
+        """Return the current database session."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
-        ''' def add user '''
-        user = User(email=email, hashed_password=hashed_password)
+    def add_user(
+            self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database."""
+        user = User(
+            email=email,
+            hashed_password=hashed_password
+        )
         self._session.add(user)
         self._session.commit()
         return user
 
-    def find_user_by(self, **kargs) -> TypeVar('User'):
-        ''' def find user '''
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments."""
         try:
-            user = self._session.query(User).filter_by(**kargs).first()
+            user = self._session.query(User).filter_by(**kwargs).first()
         except TypeError:
             raise InvalidRequestError
         if user is None:
@@ -44,11 +48,13 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        ''' def update user '''
+        """Update a user."""
         user = self.find_user_by(id=user_id)
-        for x, y in kwargs.items():
-            if hasattr(user, x):
-                setattr(user, x, y)
+
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
             else:
                 raise ValueError
+
         self._session.commit()
